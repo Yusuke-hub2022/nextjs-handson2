7b8ea8ae7fb596d1e545a7117749e73da5ad8a6f
FirstName LastName <lastname_firstname@example.com>
2024-09-16 00:42:37 +0900

    Resolve a unused variable.

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -4,6 +4,7 @@ export async function POST(request: Request) {
     const { query }: { query: unknown } = await request.json();
     if (!query || typeof query !== 'string') {
         const response = new Response('no query', { status: 400 });
+        return response;
     }
     const searchPhotosResponse = await searchPhotos(query as string);
     //console.log(searchPhotosResponse);
